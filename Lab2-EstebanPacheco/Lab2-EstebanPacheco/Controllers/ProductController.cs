721ab67901b467c74259e10a2da61b32fb9e7fab
Esteban Mathías Pacheco Carrión <167585673+Estebanpacheco18@users.noreply.github.com>
2025-08-28 12:54:52 -0500

    Add new project

---
+++ Lab2-EstebanPacheco/Lab2-EstebanPacheco/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2_EstebanPacheco.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        static List<string> list = new List<string> { "Laptop", "Mouse", "Phone" };
+
+        [HttpGet("ListAllProducts")]
+        public IActionResult ListAllProducts()
+        {
+            return Ok(list);
+        }
+
+        [HttpGet("ListSelected/{id}")]
+        public IActionResult ListSelected(int id)
+        {
+            if (id < 0 || id >= list.Count)
+            {
+                return NotFound("Product not found");
+            }
+
+            string selectedProduct = list[id];
+            list.RemoveAt(id);
+            return Ok($"Selected product: {selectedProduct}");
+        }
+    }
+}
